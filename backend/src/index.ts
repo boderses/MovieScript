82480b4be6add4d6d39d7a2ede5fbc61df652db1
Bohdan Ses <91324589+boderses@users.noreply.github.com>
2024-09-23 19:56:20 +0200

    feat: add movie model, service, controller and type, add mixin, http service and base controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import router from "./routes";
+import 'reflect-metadata';
 import dotenv from 'dotenv';
 
 dotenv.config();
