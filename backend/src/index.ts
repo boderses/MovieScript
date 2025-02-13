3f24e42d5416377e5f711b78a8eb840a8d2008d6
Bohdan Ses <91324589+boderses@users.noreply.github.com>
2025-02-13 20:24:50 +0100

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,7 +20,7 @@ declare global {
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
